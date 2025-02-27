fe316bdeb3345a4a89e72c319e6de2978c695934
최원빈 <wonbin109@naver.com>
2025-02-27 22:22:19 +0900

    mock 서버 구축

---
+++ src/app/login/RegisterModal.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+import React from 'react';
+
+interface RegisterModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const RegisterModal = () => {
+  return <div></div>;
+};
+
+export default RegisterModal;
